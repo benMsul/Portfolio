b4f80813620bc9a67f2694d9d982ab44ff30c89b
benMsul <benMsul>
2023-08-01 15:06:42 +0200

    react Portfolio

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss('./tailwind.config.js')],
-  base: "https://benMsul.github.io/porfolio"
-})
+  base: '/portfolio/',
+});
+
